3427287f95c2fa2203848be0051c21e8567c0a38
Dan Powell <powell956@gmail.com>
2019-01-07 16:17:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,6 +40,7 @@ end
 def bulk_tweet_shortener(tweets)
   tweets.map! { |tweet| word_substituter(tweet) }
   tweets.each { |tweet| puts tweet }
+  tweets
 end
 
 tweet_one = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
