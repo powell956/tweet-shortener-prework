47b7ded4d743d75e5aacd61839bf06c1074624d7
Dan Powell <powell956@gmail.com>
2019-01-07 15:13:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,27 +38,27 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
-  tweets.each { |tweet| word_substituter(tweet)}
+  tweets.map { |tweet| word_substituter(tweet)}
   binding.pry
   tweets.each { |tweet| puts tweet }
 end
 
-tweet_one = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-tweet_two = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
-tweet_three = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-tweet_four = "New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"
-tweet_five = "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
-tweets = [tweet_one, tweet_two, tweet_three, tweet_four, tweet_five]
-bulk_tweet_shortener(tweets)
-
-
-def hamburger(toppings)
-  i = 0
-  toppings = toppings.each do |topping|
-    placeholder = topping
-    toppings.delete_at(i)
-    toppings.insert(i, "I love #{placeholder} on my burgers!")
-    i += 1
-  end
-  toppings
-end
+# tweet_one = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+# tweet_two = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
+# tweet_three = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+# tweet_four = "New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"
+# tweet_five = "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
+# tweets = [tweet_one, tweet_two, tweet_three, tweet_four, tweet_five]
+# bulk_tweet_shortener(tweets)
+#
+#
+# def hamburger(toppings)
+#   i = 0
+#   toppings = toppings.each do |topping|
+#     placeholder = topping
+#     toppings.delete_at(i)
+#     toppings.insert(i, "I love #{placeholder} on my burgers!")
+#     i += 1
+#   end
+#   toppings
+# end
